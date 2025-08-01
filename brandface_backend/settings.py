dadb95b672192a8f2b12085fbb8bab4a0e6823d8
Developer <vishnuvvp007@gmail.com>
2025-08-01 20:54:18 +0530

    settings changes

--- brandface_backend/settings.py
+++ brandface_backend/settings.py
@@ -240,12 +240,21 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')  # e.g., 'us-east-1', 'eu-west-1'
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_DEFAULT_ACL = None  # Default access control list (None means private files)
+AWS_QUERYSTRING_AUTH = False  # To make files publicly accessible (Optional)
+
 
 # Static files settings
 STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/static/'
 
 # Use S3 to store static files
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Use S3 for static files and media files
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 # Optional: Control whether you want to use caching or not
 AWS_S3_OBJECT_PARAMETERS = {
